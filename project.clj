f5e89cb0e259e327a11b7603d488377fc80736e0
Alexis Lee <alee@greshamtech.com>
2018-05-17 15:49:13 +0100

    Set unsigned deployment

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject org.clojars.lxsli/lein-test-out "0.3.2"
             :eval-in-leiningen true
-            :dependencies [[org.clojure/tools.namespace "0.2.3"]])
+            :dependencies [[org.clojure/tools.namespace "0.2.3"]]
+            :deploy-repositories [["releases" {:sign-releases false
+                                               :url "https://clojars.org/repo"}]])
